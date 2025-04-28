83a00079fdbecfd19d51c105cdbdd0f59043a0d7
Kevin Prudente <kprudente@mparticle.com>
2025-04-28 15:07:08 -0400

    ci: cleaning up what's in the npm package

--- sdk/typescript-schema/index.ts
+++ sdk/typescript-schema/index.ts
@@ -11,8 +11,19 @@ export * from './query/user-query';
 
 // Export expression types
 export * from './expression/expression';
+export * from './expression/count-expression';
+export * from './expression/date-expression';
+export * from './expression/location-expression';
 export * from './expression/path-expression';
 export * from './expression/single-model-expression';
 
+// Export operand types
+export * from './operand/operand';
+export * from './operand/location-operand';
+export * from './operand/date-operand';
+
 // Export common types
+export * from './common/location'; 
+export * from './common/model'; 
 export * from './common/operator'; 
+export * from './common/version'; 
---
+++ sdk/typescript-schema/scripts/prepare-dist.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+# Check if dist directory exists
+if [ -d "dist" ]; then
+    # Copy LICENSE file to dist directory
+    cp -f ../../LICENSE dist/
+
+    # Copy package.json to dist directory
+    cp -f package.json dist/
+
+    # Copy README.md to dist directory
+    cp -f README.md dist/
+
+    # Overwrite any version file
+    VERSION=$(node -p "require('./package.json').version")
+    echo "export const VERSION = '$VERSION';" > version.ts
+
+    # Compile version.ts to JavaScript and move to dist directory
+    npx tsc version.ts --outDir dist --emitDeclarationOnly true --declaration true
+fi
