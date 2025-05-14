604954fd17b52febbcd557300f80c83f7a55400f
Kevin Prudente <kprudente@mparticle.com>
2025-05-14 11:09:08 -0400

    Schemas are now generated consistently with titles

---
+++ sdk/typescript-schema/scripts/gen_schema_with_titles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e  # Exit on errors
+
+TSCONFIG="tsconfig.json"
+
+# Use a trap to ensure we reset emitDeclarationOnly to true no matter what
+function restore_tsconfig {
+  echo "Restoring emitDeclarationOnly to true..."
+  jq '.compilerOptions.emitDeclarationOnly = true' "$TSCONFIG" > tmp.tsconfig.json && mv tmp.tsconfig.json "$TSCONFIG"
+}
+trap restore_tsconfig EXIT
+
+echo "Setting emitDeclarationOnly to false..."
+jq '.compilerOptions.emitDeclarationOnly = false' "$TSCONFIG" > tmp.tsconfig.json && mv tmp.tsconfig.json "$TSCONFIG"
+
+echo "Generating JSON schema..."
+./node_modules/.bin/ts-json-schema-generator --path index.ts --out schema/audience-schema.json
+
+echo "Adding titles to schema..."
+./scripts/add_titles_to_schema.sh schema/audience-schema.json > ../../schema/audience-definition-schema.json
+
+echo "Schema generation complete."
